7e4a8ff1de6e7a4ec44ae4cbf74cb9e1d9cced4d
Anna Kazak <anna.s.kazak@gmail.com>
2024-07-03 12:23:39 +0200

    Add commit 1 <p>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,4 +7,26 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-process
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a>
+            className="App-link"
+            href="https://reactjs2.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            Learn React
+            <p>blabla</p>
+          </a>
+          <p>11</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;

60ba157b19466df5787d2a03f5261c9ca6583242
Anna Kazak <anna.s.kazak@gmail.com>
2024-07-04 13:03:28 +0200

    dev branch commit3

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -26,6 +26,7 @@ function App() {
           <div>dev branch commit</div>
           <div>dev branch commit1</div>
           <div>dev branch commit2</div>
+          <div>dev branch commit3</div>
           <p>11</p>
           <p>2</p>
           <p>3</p>
